deae002d6ca596d3eec2f37c57d06aa466e9051c
Girls Who Code <girlswhocode@Girlss-MacBook-Pro.local>
2017-07-31 14:32:55 -0400

    Comment section fixed

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
-    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]

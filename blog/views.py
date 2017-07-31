7ba1d981fb9d5748012875e8e670375d86c024c0
Girls Who Code <girlswhocode@Girlss-MacBook-Pro.local>
2017-07-31 10:40:29 -0400

    Fixed order

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_detail(request, pk):
